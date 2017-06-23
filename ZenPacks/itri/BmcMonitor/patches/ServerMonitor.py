8f5ac3c3fb620b020969a91774cc4b7fd69cee4c
Andres Alvarez <jalvarez@itri.org.tw>
2017-06-23 17:39:24 +0800

    Enable powerStatus in the API

--- ZenPacks/itri/BmcMonitor/patches/ServerMonitor.py
+++ ZenPacks/itri/BmcMonitor/patches/ServerMonitor.py
@@ -1,12 +1,16 @@
 import logging
 log = logging.getLogger('zen.BmcMonitor')
 
-from Products.ZenUtils.Utils import monkeypatch
-from ZenPacks.itri.ServerMonitor.ItriServer import ItriServer
+from Products.Zuul.infos import ProxyProperty
+
+from ZenPacks.itri.ServerMonitor.ItriServer import ItriServer, ItriServerInfo
 
 
 # Add power_status property
-ItriServer.power_status = False
+ItriServer.powerStatus = False
 ItriServer._properties += (
-    {'id': 'power_status', 'type': 'boolean', 'mode': 'w'},
+    {'id': 'powerStatus', 'type': 'boolean', 'mode': 'w'},
     )
+
+# Make the property available through the API
+ItriServerInfo.powerStatus = ProxyProperty('powerStatus')
