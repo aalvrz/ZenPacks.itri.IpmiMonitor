0384d3376a8d746879200dc89673aeed72f3fb29
Andres Alvarez <jalvarez@itri.org.tw>
2017-06-23 14:55:35 +0800

    Rework the modeler plugin

--- ZenPacks/itri/BmcMonitor/modeler/plugins/itri/Bmc.py
+++ ZenPacks/itri/BmcMonitor/modeler/plugins/itri/Bmc.py
@@ -1,50 +1,44 @@
 """Collects BMC information"""
 
-# Twisted Imports
 from twisted.internet.defer import inlineCallbacks, returnValue
-from twisted.web.client import getPage
-
-# Zenoss Imports
 from Products.DataCollector.plugins.CollectorPlugin import PythonPlugin
+from ZenPacks.itri.BmcMonitor.lib.ipmitool import get_power_status
 
-import subprocess
-import re
 
 class Bmc(PythonPlugin):
     """Modeler plugin for BMC servers."""
 
-    modname = 'ZenPacks.itri.BmcMonitor.BmcServer'
-    
+    relname = 'bmcServer'
+    modname = 'ZenPacks.itri.ServerMonitor.ItriServer'
+
     deviceProperties = PythonPlugin.deviceProperties + (
         'zBmcAddress',
         'zIpmiUsername',
         'zIpmiPassword',
         )
-        
+
     @inlineCallbacks
     def collect(self, device, log):
-        log.info('Modeler {0} collecting data for device {1}'.format(self.name(), device.id))        
+        log.info('{0}: collecting data for device {1}'.format(
+            self.name(), device.id))
 
         bmc_address = getattr(device, 'zBmcAddress', None)
         ipmi_username = getattr(device, 'zIpmiUsername', None)
         ipmi_password = getattr(device, 'zIpmiPassword', None)
 
-        cmd_result = ''        
         try:
-            cmd = 'ipmitool -H {0} -I lanplus -U {1} -P {2} power status'.format(bmc_address, ipmi_username, ipmi_password)
-            cmd_result = yield subprocess.check_output(cmd, shell=True).rstrip()
-            log.info('Power Status for Device {0}: {1}'.format(bmc_address, cmd_result))
-        except:
-            log.error('Error when running ipmitool when collecting Power Status on BMC Address {0}'.format(bmc_address))
+            power_status = yield get_power_status(
+                bmc_address, ipmi_username, ipmi_password)
 
-        # Parse the result
-        power_status = False       
-        if cmd_result == 'Chassis Power is on':
-            power_status = True
-        
+            log.info('Power Status for device {0}: {1}'.format(
+                bmc_address, power_status))
+        except Exception as e:
+            log.error('{0}: {1}'.format(bmc_address, e))
+            returnValue(None)
+
         returnValue(self.objectMap({
             'power_status': power_status,
-        }))
-        
+            }))
+
     def process(self, device, results, log):
         return results
