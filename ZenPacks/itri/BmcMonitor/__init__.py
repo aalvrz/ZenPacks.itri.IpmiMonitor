e08bde545e4543d05f8aa1d02a3bab71a2b33b91
Andres Alvarez <jalvarez@itri.org.tw>
2017-06-27 10:42:17 +0800

    Upgrade to zenpacklib 2

--- ZenPacks/itri/BmcMonitor/__init__.py
+++ ZenPacks/itri/BmcMonitor/__init__.py
@@ -1,19 +1,40 @@
-from . import zenpacklib
-
+from ZenPacks.zenoss.ZenPackLib import zenpacklib
 CFG = zenpacklib.load_yaml()
 
-class ZenPack(schema.ZenPack):
-    def install(self, app):
-        try:
-            server_dc = self.dmd.Devices.getOrganizer('/Server')
-        except Exception:
-            pass
-        else:
-            bmc_plugin = ['itri.Bmc']
-            ps = list(server_dc.zCollectorPlugins) + bmc_plugin
-            self.device_classes['/Server'].zProperties['zCollectorPlugins'] = ps
+import logging
+log = logging.getLogger('zen.BmcMonitor')
 
-            super(ZenPack, self).install(app)
+schema = CFG.zenpack_module.schema
+
+BMC_PLUGINS = ['itri.Bmc']
+
+BMC_TEMPLATES = ['BMC']
+
+class ZenPack(schema.ZenPack):
+
+    def install(self, app):
+        self._update_plugins('/Server/SSH/Linux/NovaHost')
+        self._update_templates('/Server/SSH/Linux/NovaHost')
+
+        super(ZenPack, self).install(app)
+
+    def _update_plugins(self, organizer):
+        try:
+            dc = self.dmd.Devices.getOrganizer(organizer)
+        except Exception as e:
+            log.error(e)
+        else:
+            dc.setZenProperty( 
+                'zCollectorPlugins', dc.zCollectorPlugins + BMC_PLUGINS)
+
+    def _update_templates(self, organizer):
+        try:
+            dc = self.dmd.Devices.getOrganizer(organizer)
+        except Exception as e:
+            log.error(e)
+        else:
+            dc.setZenProperty(
+                'zDeviceTemplates', dc.zDeviceTemplates + BMC_TEMPLATES)
 
 # Patch last to avoid import recursion problems
 from . import patches
