d4af8b6a656f84fcac81fe31e83989ba3de4ace9
Andres Alvarez <jalvarez@itri.org.tw>
2017-06-28 16:02:38 +0800

    Add ipmi monitoring parser

--- ZenPacks/itri/IpmiMonitor/lib/ipmitool.py
+++ ZenPacks/itri/IpmiMonitor/lib/ipmitool.py
@@ -7,10 +7,23 @@ def get_power_status(ip_address, username='admin', password='admin'):
 
     try:
         r = subprocess.check_output(cmd, shell=True).rstrip()
-    except subprocess.CalledProcessError as e:
+    except subprocess.CalledProcessError:
         raise
 
     if r == 'Chassis Power is on':
         return True
 
     return False
+    
+def parse_ipmi(output):
+    """Parse ipmitool output into a dictionary"""
+    
+    result = {}
+
+    for row in output.split('\n'):
+        if '|' in row:
+            dp = row.split('|')[0].lower().rstrip()
+            val = float(row.split('|')[-1].split()[0].rstrip())
+            result[dp] = val
+
+    return result
